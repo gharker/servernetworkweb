6d4cf6e2921801327546e61b6d77ec2965c752a9
gharker <docharker@gmail.com>
2026-05-06 14:15:23 -0400

    fix prisma typescription bug

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    url: process.env["DIRECT_URL"],
   },
   datasource: {
     url: process.env["DATABASE_URL"],
