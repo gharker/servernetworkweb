98f0d67f8fb9b34b56c483455f260d0c94218eee
gharker <docharker@gmail.com>
2026-05-06 11:49:23 -0400

    typescript error

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -44,7 +44,7 @@ export default function MessagesPage() {
               filters={filters} 
               sort={sort}
               options={{ limit: 30 }}
-              Paginator={(props) => <>{props.children}</>}
+              Paginator={(props: any) => <>{props.children}</>}
             />
           </div>
           <div className="flex-1 flex flex-col min-w-0 bg-white">
